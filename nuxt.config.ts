565cac93253e7c848604b54246d3e6519a59023b
yu <wgy17645466412@163.com>
2026-01-06 15:10:50 +0800

    github构建

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  experimental: {
+    payloadExtraction: false,
+  },
   devtools: { enabled: true },
   app: {
-    baseURL: "/", // 部署在子路径时修改此配置，如 "/help/"
+    baseURL: "/help_center/",
+    buildAssetsDir: "nuxt_assets",
   },
   modules: [
     "@nuxt/content",
@@ -15,20 +19,14 @@ export default defineNuxtConfig({
   ],
   css: ["~/assets/css/main.css"],
   content: {},
-
-  // Nuxt Image 静态配置 - 禁用 IPX 处理器
   image: {
     provider: "none",
   },
-
-  // 静态部署配置
-  ssr: true, // 禁用服务端渲染,生成纯静态站点
-
+  ssr: true,
   nitro: {
-    preset: "static", // 使用静态预设
+    preset: "github-pages",
   },
   routeRules: {
-    // 对所有路由进行静态预渲染
     "/**": { prerender: true },
   },
 });
